8d1d9ace6db6bba43eb8d06c2a82daa52f834a39
Ji ZHANG <jizhang@anjuke.com>
2014-05-29 10:52:26 +0800

    button group

--- src/main/webapp/js/Runner.js
+++ src/main/webapp/js/Runner.js
@@ -1,48 +1,69 @@
-define("/explorer/js/Runner.js", [
-    "dojo",
-    "dijit",
-    "dijit/_editor/_Plugin",
-    "dijit/form/Button"
-], function(dojo, dijit, _Plugin, Button) {
+define('explorer/Runner', [
+    'dojo/_base/declare',
+    'dojo/_base/lang',
+    'dojo/_base/array',
+    'dijit/_editor/_Plugin',
+    'dijit/form/Button'
+], function(declare, lang, array, _Plugin, Button) {
 
-    var Runner = dojo.declare("Runner", _Plugin, {
+    var Runner = declare('explorer.Runner', _Plugin, {
 
-        _initButton: function() {
-            var editor = this.editor,
-                iconClass = '';//this.iconClassPrefix + " " + this.iconClassPrefix + "Runner";
-            this.button = new Button({
-                label: "Run Selected",
+        buttonGroup: [],
+
+        _initButtonGroup: function() {
+            var editor = this.editor;
+
+            var props = {
                 showLabel: true,
-                iconClass: iconClass,
+                iconClass: '',
                 ownerDocument: editor.ownerDocument,
                 dir: editor.dir,
                 lang: editor.lang,
-                tabIndex: '-1',
-                onClick: dojo.hitch(this, "_runSelected")
-            });
+                tabIndex: '-1'
+            };
+
+            this.buttonGroup.push(new Button(lang.mixin(props, {
+                label: 'Run Selected',
+                onClick: lang.hitch(this, '_runSelected')
+            })));
+
+            this.buttonGroup.push(new Button(lang.mixin(props, {
+                label: 'Run All',
+                onClick: lang.hitch(this, '_runAll')
+            })));
+
         },
 
         setEditor: function(editor) {
             this.editor = editor;
-            this._initButton();
+            this._initButtonGroup();
+        },
+
+        setToolbar: function(toolbar) {
+            array.forEach(this.buttonGroup, function(button) {
+                toolbar.addChild(button);
+            });
         },
 
         updateState: function() {
-            this.button.set('disabled', this.get('disabled'));
+            var runner = this;
+            array.forEach(this.buttonGroup, function(button) {
+                button.set('disabled', runner.get('disabled'));
+            });
         },
 
         _runSelected: function() {
-            console.log("run selected");
+            console.log('run selected');
         },
 
         _runAll: function() {
-            console.log("run all");
+            console.log('run all');
         }
 
     });
 
-    _Plugin.registry["runner"] = function(args) {
-        return new Runner({});
+    _Plugin.registry['runner'] = function(args) {
+        return new Runner();
     };
 
     return Runner;

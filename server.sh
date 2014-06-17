7c5deb459fa4738a3b6b154cccf162966c931f3b
Ji ZHANG <jizhang@anjuke.com>
2014-06-17 17:22:49 +0800

    use cms gc

--- server.sh
+++ server.sh
@@ -2,4 +2,5 @@
 
 git pull
 ./sbt package
-java -jar /data2/data-profiling/jetty-runner-8.1.11.v20130520.jar --port 9081 --path /explorer /data2/data-profiling/dw_explorer/target/scala-2.10/dw-explorer_2.10-0.1.0-SNAPSHOT.war
+java -server -Xmx1024M -XX:MaxPermSize=128M -XX:+UseConcMarkSweepGC -XX:+CMSClassUnloadingEnabled \
+  -jar /data2/data-profiling/jetty-runner-8.1.11.v20130520.jar --port 9081 --path /explorer /data2/data-profiling/dw_explorer/target/scala-2.10/dw-explorer_2.10-0.1.0-SNAPSHOT.war

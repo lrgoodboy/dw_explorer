cd09481fdc9705db3a28110c76a606fe274961d0
Ji ZHANG <jizhang@anjuke.com>
2014-09-25 11:20:46 +0800

    update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-cp target/scala-2.10/dw-explorer_2.10-0.1.0-SNAPSHOT.war jetty-distribution-8.1.15.v20140411/webapps/explorer.war
+scp target/scala-2.10/dw-explorer_2.10-0.1.0-SNAPSHOT.war root@10.20.8.38:/data2/dw_explorer/jetty-distribution-8.1.16.v20140903/webapps/explorer.war
 
-SUPERVISOR_HOME=/data2/data-profiling/supervisor
-$SUPERVISOR_HOME/venv/bin/supervisorctl -c $SUPERVISOR_HOME/supervisord.conf restart dw_explorer
+ssh root@10.20.8.38 supervisorctl restart dw_explorer
